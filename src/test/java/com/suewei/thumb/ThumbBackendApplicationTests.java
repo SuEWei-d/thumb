6e2a8508884780a5fd4f83483d3a945f22589bed
SuEWei <2941794982@qq.com>
2026-03-31 18:24:08 +0800

    Initial commit

---
+++ src/test/java/com/suewei/thumb/ThumbBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suewei.thumb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThumbBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
